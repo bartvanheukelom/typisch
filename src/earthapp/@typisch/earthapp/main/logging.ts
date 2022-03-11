5adb87c94a7f258e12264ffcc9e0f50141fe2fb8
Bart van Heukelom <mail@bartvh.nl>
2022-03-11 16:22:13 +0100

    -

--- src/earthapp/@typisch/earthapp/main/logging.ts
+++ src/earthapp/@typisch/earthapp/main/logging.ts
@@ -1,16 +1,31 @@
 
-export function pimpConsoleLog() {
+export function pimpConsoleLog(): void {
     const orgLog = console.log
-    console.log = (...data: any[]) => {
-        const parts = [new Date().toISOString()]
-        parts.push(...data.map(d => {
-            switch (typeof d) {
-                case "undefined": return "undefined";
-                case "object": return JSON.stringify(d);
-                case "string": return d;
-                default: return d.toString();
-            }
-        }))
-        orgLog(parts.join(" "))
+    console.log = (...data: unknown[]) => {
+        let msg: string;
+        try {
+            const parts = [new Date().toISOString()];
+            parts.push(...data.map(d => {
+                try {
+                    switch (typeof d) {
+                        case "undefined": return "undefined";
+                        case "object": return JSON.stringify(d);
+                        case "string": return d;
+                        default: return `${d}`;
+                    }
+                } catch (e) {
+                    return `[ERROR in toString: ${e}]`;
+                }
+            }));
+            msg = parts.join(" ");
+        } catch (e) {
+            msg = `pimpConsoleLog error: ${e}`;
+        }
+        try {
+            orgLog(msg);
+        } catch (e) {
+            process.stderr.write(`Error in console.log: ${e}`, 'utf-8');
+            process.stderr.write(`Message: ${msg}`, 'utf-8');
+        }
     }
 }
