e16be5b34e3a767cd82e4e5537f8b3ea200cd412
Bart van Heukelom <mail@bartvh.nl>
2022-03-14 15:38:20 +0100

    abyss

--- src/core/@typisch/core/fun.ts
+++ src/core/@typisch/core/fun.ts
@@ -2,3 +2,11 @@
 export function run<T>(f: () => T): T {
     return f()
 }
+
+/**
+ * Function that accepts any args and does... nothing.
+ * The functional equivalent of writing to `/dev/null`.
+ * It's like a void, but that has a specific meaning in JS.
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-empty-function
+export function abyss(...args: unknown[]): void {}
