2eae34faf00a0837994c467b562e2e6b61e3e735
Bart van Heukelom <mail@bartvh.nl>
2022-10-06 19:01:48 +0200

    tsconfig-base

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,10 +1,8 @@
 import {typischCoreNop} from "@typisch/core";
-import {typischReactNop} from "@typisch/react";
 
 typischCoreNop();
-typischReactNop();
 
 /**
  * Does nothing, can be used as an early detector for import / module resolution issues.
  */
-export function typischEuiNop() {}
+export function typischReactNop() {}
