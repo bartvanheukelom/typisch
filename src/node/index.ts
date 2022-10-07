a4f791d8c14347f803e0a8391b3d4e482f9c7541
Bart van Heukelom <mail@bartvh.nl>
2022-10-07 15:08:34 +0200

    tsconfig refactors and fixes for standalone build

---
+++ src/node/index.ts
@@ -0,0 +1,8 @@
+import {typischCoreNop} from "@typisch/core";
+
+typischCoreNop();
+
+/**
+ * Does nothing, can be used as an early detector for import / module resolution issues.
+ */
+export function typischNodeNop() {}
