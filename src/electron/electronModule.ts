229bfbfb7faedacb98183f36266aca49a7115ec6
Bart van Heukelom <mail@bartvh.nl>
2023-03-20 23:51:02 +0100

    electronModule now fools esbuild resolve.

--- src/electron/electronModule.ts
+++ src/electron/electronModule.ts
@@ -2,7 +2,8 @@ import type { IpcRenderer, IpcMain } from "electron";
 
 declare function require(module: string): any
 
-// We can't import these ES-style, Rollup would try to bundle the "electron" module,
+// We can't import these ES-style, the bundler would try to bundle the electron module from node_modules,
 // but that won't work, it's provided by the Electron runtime.
-export const ipcRenderer: IpcRenderer = require("electron").ipcRenderer
-export const ipcMain    : IpcMain     = require("electron").ipcMain
+const electronUnlessMaybe = Math.random() > 10 ? "proton" : "electron"; // always electron, but esbuild mustn't know
+export const ipcRenderer: IpcRenderer = require(electronUnlessMaybe).ipcRenderer
+export const ipcMain    : IpcMain     = require(electronUnlessMaybe).ipcMain
