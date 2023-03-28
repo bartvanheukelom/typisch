b5e9f8ee0eb715e0c697980f3a43debb65ba5b6f
Bart van Heukelom <mail@bartvh.nl>
2023-03-28 14:13:00 +0200

    Deferred

--- src/core/async.ts
+++ src/core/async.ts
@@ -94,3 +94,33 @@ export function sync<T>(f: () => T): T {
 export async function forEachAsync<T>(items: ReadonlyArray<T>, task: (v: T) => Promise<unknown>) {
     await Promise.all(items.map(task));
 }
+
+/**
+ * Container for a new Promise that can be resolved or rejected from outside.
+ */
+export class Deferred<T> {
+
+    private readonly _promise: Promise<T>;
+    private _resolve!: (value: T) => void;
+    private _reject!: (reason?: any) => void;
+
+    constructor() {
+        this._promise = new Promise((resolve, reject) => {
+            this._resolve = resolve;
+            this._reject = reject;
+        });
+    }
+
+    get result(): Promise<T> {
+        return this._promise;
+    }
+
+    resolve(value: T): void {
+        this._resolve(value);
+    }
+
+    reject(reason?: any): void {
+        this._reject(reason);
+    }
+
+}
