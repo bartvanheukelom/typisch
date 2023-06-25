968409f4a5f5c47401f6c0b6867aef8ab711eeb3
Bart van Heukelom <mail@bartvh.nl>
2023-06-25 14:40:30 +0200

    sqlStringLiteral doc

--- src/core/sql/index.ts
+++ src/core/sql/index.ts
@@ -1,4 +1,13 @@
 
+
+/**
+ * Convert a string to an SQL string literal.
+ * Escapes single quotes and wraps the whole thing in single quotes.
+ * Other characters are not escaped, including \n, which is actually a valid character in SQL string literals.
+ *
+ * `Yoshi's Story` -> `'Yoshi''s Story'`
+ * `hello⎆world` -> `'hello⎆world'`
+ */
 export function sqlStringLiteral(str: string) {
     return str.split("'").map(p => `'${p}'`).join("")
 }
