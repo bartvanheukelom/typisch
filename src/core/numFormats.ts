418f419fda3fd64fd6595a22795e750c8a4beda7
Bart van Heukelom <mail@bartvh.nl>
2023-12-13 21:09:02 +0100

    formatFileSize: autorange, round mode, SI/IEC choice, ' as group sep

--- src/core/numFormats.ts
+++ src/core/numFormats.ts
@@ -1,6 +1,7 @@
+import {declareIs} from "./utils";
 
-const siPrefixes = {
-    "K": 1e3,
+export const siPrefixes = {
+    "k": 1e3,
     "M": 1e6,
     "G": 1e9,
     "T": 1e12,
@@ -9,16 +10,61 @@ const siPrefixes = {
     "Z": 1e21,
     "Y": 1e24,
 }
+export type SIPrefix = keyof typeof siPrefixes;
 
-export function formatFileSize(size: number, unit: "B" | "KB" | "MB" | "GB" = "B"): string {
+export const iecPrefixes = {
+    "Ki": 1024,
+    "Mi": 1024 ** 2,
+    "Gi": 1024 ** 3,
+    "Ti": 1024 ** 4,
+    "Pi": 1024 ** 5,
+    "Ei": 1024 ** 6,
+    "Zi": 1024 ** 7,
+    "Yi": 1024 ** 8,
+}
+export type IECPrefix = keyof typeof iecPrefixes;
+
+export function formatFileSize(
+    size: number,
+    unit: `${">" | "<" | ""}${SIPrefix | IECPrefix | "*" | "*i" | ""}B` = "B",
+): string {
     let num = size;
-    if (unit != "B") {
-        const div = siPrefixes[unit[0] as keyof typeof siPrefixes];
-        if (!div) {
-            throw new Error(`invalid unit ${unit}`)
-        }
-        num = Math.round(num / div);
+    
+    let unitt: string = unit;
+    declareIs<never>(unit);
+
+    let round = Math.round;
+    if (unitt[0] == ">") {
+        round = Math.floor;
+        unitt = unitt.slice(1);
+    } else if (unitt[0] == "<") {
+        round = Math.ceil;
+        unitt = unitt.slice(1);
     }
 
-    return `${num.toLocaleString()} ${unit}`;
+    if (unitt != "B") {
+
+        let prefix = unitt.slice(0, -1);
+        if (prefix[0] == "*") {
+            let prefixes = [["", 1] as const, ...Object.entries(prefix == "*" ? siPrefixes : iecPrefixes)];
+            // pick the prefix where the rounded number has 2 through 4 digits
+            let i = prefixes.findIndex(([_, div]) => {
+                let n = round(num / div);
+                return n >= 10 && n <= 9999;
+            });
+            prefix = prefixes[i == -1 ? (num < 10 ? 0 : prefixes.length - 1) : i][0];
+        }
+
+        const div = prefix == "" ? 1 : siPrefixes[prefix as SIPrefix] ?? iecPrefixes[prefix as IECPrefix];
+        if (!div) {
+            throw new Error(`no div found for prefix '${prefix}'`);
+        }
+        unitt = prefix + "B";
+        num = num / div;
+    }
+
+    // use ' as thousands separator
+    const numStr = round(num).toLocaleString().replace(/[,.]/g, "'"); // TODO yuk
+
+    return `${numStr} ${unitt}`;
 }
